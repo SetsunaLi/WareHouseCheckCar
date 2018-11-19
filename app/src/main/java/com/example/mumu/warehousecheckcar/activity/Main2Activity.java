0fb7d0e003f23968d7a63bafbca10d768e18181a
mumu <13630064412@163.com>
2018-11-19 16:02:49 +0800

    空框架

--- app/src/main/java/com/example/mumu/warehousecheckcar/activity/Main2Activity.java
+++ app/src/main/java/com/example/mumu/warehousecheckcar/activity/Main2Activity.java
@@ -2,9 +2,8 @@ package com.example.mumu.warehousecheckcar.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,38 +11,79 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.mumu.warehousecheckcar.R;
+import com.example.mumu.warehousecheckcar.fragment.OptionMenu;
+import com.example.mumu.warehousecheckcar.picture.CutToBitmap;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class Main2Activity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Bind(R.id.content_frame)
+    FrameLayout contentFrame;
+    @Bind(R.id.nav_view)
+    NavigationView navView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
 
+        //右下角按钮监听
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "欢迎使用亿锋公司配套软件", Snackbar.LENGTH_SHORT)
                         .setAction("Action", null).show();
             }
         });
-
+//       左上角按钮
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        initDate();
+        initView();
     }
 
+    //    导航数组
+    private String[] mOptionTitle;
+
+    private void initDate() {
+        mOptionTitle = getResources().getStringArray(R.array.options_array);
+    }
+
+    private void initView() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+//        导航头部
+        ImageView iv = (ImageView) navigationView.getHeaderView(0).findViewById(R.id.imageView);
+        iv.setImageBitmap(CutToBitmap.changeToBitmap(getResources(), iv, R.mipmap.user_head));
+        TextView tv1 = (TextView) navigationView.getHeaderView(0).findViewById(R.id.textView1);
+        tv1.setText(getResources().getString(R.string.options_username));
+        TextView tv2 = (TextView) navigationView.getHeaderView(0).findViewById(R.id.textView2);
+        tv2.setText(getResources().getString(R.string.options_welcome));
+//      导航列表(枚举)
+        for (OptionMenu om : OptionMenu.values()) {
+            MenuItem mi = navigationView.getMenu().findItem(om.getId());
+            mi.setTitle(mOptionTitle[om.getIndex()]);
+        }
+    }
+
+    //返回键监听
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -54,6 +94,7 @@ public class Main2Activity extends AppCompatActivity
         }
     }
 
+    //右上角列表R.menu.main2
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -61,6 +102,7 @@ public class Main2Activity extends AppCompatActivity
         return true;
     }
 
+    //右上角列表点击监听（相当于onclickitemlistener,可用id或者title匹配）
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -76,28 +118,39 @@ public class Main2Activity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+    //左边列表点击监听（相当于onclickitemlistener,可用id匹配）
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_item1) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_item2) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_item3) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_item4) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_item5) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_item6) {
+
+        } else if (id == R.id.nav_item7) {
+
+        } else if (id == R.id.nav_about) {
 
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ButterKnife.unbind(this);
     }
 }
---
+++ app/src/main/java/com/example/mumu/warehousecheckcar/fragment/OptionMenu.java
@@ -0,0 +1,34 @@
+package com.example.mumu.warehousecheckcar.fragment;
+
+import com.example.mumu.warehousecheckcar.R;
+
+/**
+ * Created by mumu on 2018/11/19.
+ */
+
+public enum  OptionMenu {
+    ITEM1(R.id.nav_item1,0),ITEM2(R.id.nav_item2,1),ITEM3(R.id.nav_item3,2),ITEM4(R.id.nav_item4,3)
+    ,ITEM5(R.id.nav_item5,4),ITEM6(R.id.nav_item6,5),ITEM7(R.id.nav_item7,6),ITEM8(R.id.nav_about,7);
+    private int id;
+    private int index;
+    private OptionMenu(int id,int index){
+        this.id=id;
+        this.index=index;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+}
