8e91319405a111c47782e46bff7329a3cbdf8962
mumu <13630064412@163.com>
2020-06-04 16:15:18 +0800

    11.21

--- app/src/main/java/com/example/mumu/warehousecheckcar/fragment/HomeFragment.java
+++ app/src/main/java/com/example/mumu/warehousecheckcar/fragment/HomeFragment.java
@@ -54,8 +54,8 @@ public class HomeFragment extends BaseFragment {
     Button button14;
     @Bind(R.id.button15)
     Button button15;
-    @Bind(R.id.button16)
-    Button button16;
+    @Bind(R.id.button0)
+    Button button0;
     @Bind(R.id.button2)
     Button button2;
     @Bind(R.id.button17)
@@ -71,6 +71,13 @@ public class HomeFragment extends BaseFragment {
     }
 
     private User user;
+    private Button[] buttons = new Button[]{button0, button1, button2, button3, button4, button5, button6, button7, button8,
+            button9, button10, button11, button12, button13, button14, button15, button17, button18};
+    private int[] imgs = new int[]{
+            R.mipmap.setting_l, R.mipmap.click1_l, R.mipmap.click2_l, R.mipmap.click3_l, R.mipmap.click4_l, R.mipmap.click5_l
+            , R.mipmap.click6_l, R.mipmap.click7_l, R.mipmap.click8_l, R.mipmap.click9_l, R.mipmap.click10_l, R.mipmap.click11_l
+            , R.mipmap.click12_l, R.mipmap.click13_l, R.mipmap.click14_l, R.mipmap.click15_l, R.mipmap.backin_l, R.mipmap.chubb_get_l
+    };
 
     @Nullable
     @Override
@@ -88,87 +95,88 @@ public class HomeFragment extends BaseFragment {
 
     @Override
     protected void initView(View view) {
-        for (HomeButton homeButton : HomeButton.values()) {
-            Button button = (Button) view.findViewById(homeButton.getId());
-            button.setCompoundDrawables(
-                    null,
-                    Imgutil.findImgAsSquare(getActivity(),
-                            homeButton.getIndex(), 64),
-                    null,
-                    null);
-        }
-        if (user != null&&user.getApp_auth()!=null) {
-            boolean outFlag = false;
-            for (Power power : user.getApp_auth()) {
-                Button button;
-                switch (power.getAuth_type()) {
-                    case 0:
-                        button = button1;
-                        break;
-                    case 1:
-                        button = button2;
-                        break;
-                    case 2:
-                        button = button3;
-                        break;
-                    case 3:
-                        button = button4;
-                        break;
-                    case 4:
-                        button = button5;
-                        break;
-                    case 5:
-                        button = button6;
-                        break;
-                    case 6:
-                        button = button7;
-                        if (power.getFlag() != 0)
-                            outFlag = true;
-                        break;
-                    case 7:
-                        if (outFlag)
-                            button = null;
-                        else
+        if (view != null) {
+            for (int i = 0; i < buttons.length; i++) {
+                buttons[i].setCompoundDrawables(
+                        null,
+                        Imgutil.findImgAsSquare(getActivity(),
+                                imgs[i], 64),
+                        null,
+                        null);
+            }
+            if (user != null && user.getApp_auth() != null) {
+                boolean outFlag = false;
+                for (Power power : user.getApp_auth()) {
+                    Button button;
+                    switch (power.getAuth_type()) {
+                        case 0:
+                            button = button1;
+                            break;
+                        case 1:
+                            button = button2;
+                            break;
+                        case 2:
+                            button = button3;
+                            break;
+                        case 3:
+                            button = button4;
+                            break;
+                        case 4:
+                            button = button5;
+                            break;
+                        case 5:
+                            button = button6;
+                            break;
+                        case 6:
                             button = button7;
-                        break;
-                    case 8:
-                        button = button8;
-                        break;
-                    case 9:
-                        button = button9;
-                        break;
-                    case 10:
-                        button = button10;
-                        break;
-                    case 11:
-                        button = button11;
-                        break;
-                    case 12:
-                        button = button12;
-                        break;
-                    case 13:
-                        button = button13;
-                        break;
-                    case 14:
-                        button = button14;
-                        break;
-                    case 15:
-                        button = button15;
-                        break;
-                    case 16:
-                        button = button17;
-                        break;
-                    case 17:
-                        button = button18;
-                        break;
-                    default:
-                        button = null;
-                }
-                if (button != null) {
-                    if (power.getFlag() == 0)
-                        button.setVisibility(View.GONE);
-                    else
-                        button.setVisibility(View.VISIBLE);
+                            if (power.getFlag() != 0)
+                                outFlag = true;
+                            break;
+                        case 7:
+                            if (outFlag)
+                                button = null;
+                            else
+                                button = button7;
+                            break;
+                        case 8:
+                            button = button8;
+                            break;
+                        case 9:
+                            button = button9;
+                            break;
+                        case 10:
+                            button = button10;
+                            break;
+                        case 11:
+                            button = button11;
+                            break;
+                        case 12:
+                            button = button12;
+                            break;
+                        case 13:
+                            button = button13;
+                            break;
+                        case 14:
+                            button = button14;
+                            break;
+                        case 15:
+                            button = button15;
+                            break;
+                        case 16:
+                            button = button17;
+                            break;
+                        case 17:
+                            button = button18;
+                            break;
+                        default:
+                            button = null;
+                    }
+                    if (button != null) {
+                        if (power.getFlag() == 0)
+                            button.setVisibility(View.GONE);
+                        else
+                            button.setVisibility(View.VISIBLE);
+                    }
                 }
             }
         }
